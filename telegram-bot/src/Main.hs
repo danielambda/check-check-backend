4d7ebb16e7c269f7b60d10d54b49c6651a53e9c3
danielambda <daniel.gevorgyan25@gmail.com>
2025-03-09 23:52:36 +0300

    feat: selecting items

--- telegram-bot/src/Main.hs
+++ telegram-bot/src/Main.hs
@@ -1,84 +1,188 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE NoFieldSelectors #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main (main) where
 
 import Telegram.Bot.API
-  ( BotName(..), Update, Token, defaultTelegramClientEnv, userUsername, responseResult, getMe )
-import Telegram.Bot.Simple (BotApp(..), Eff, startBot_, getEnvToken, (<#), replyText)
-import Telegram.Bot.Simple.UpdateParser (parseUpdate, command, commandWithBotName)
+  ( BotName(..), Update, Token , defaultTelegramClientEnv , userUsername
+  , responseResult, getMe, SomeReplyMarkup (SomeInlineKeyboardMarkup), InlineKeyboardMarkup (..)
+  , updateChatId
+  )
+import Telegram.Bot.Simple
+  ( BotApp(..), Eff, startBot_, getEnvToken, ReplyMessage (replyMessageReplyMarkup), reply
+  , actionButton, conversationBot, replyText
+  , editUpdateMessage, EditMessage (..), BotM, GetAction, withEffect
+  )
+import Telegram.Bot.Simple.UpdateParser
+  ( parseUpdate, command, commandWithBotName, callbackQueryDataRead )
 import Configuration.Dotenv (loadFile, defaultConfig)
-import Servant.Client (runClientM, ClientM, client, mkClientEnv, Scheme (Http), BaseUrl (BaseUrl))
+import Servant.Client (runClientM, ClientM, client, mkClientEnv, Scheme (Http), BaseUrl (BaseUrl), ClientError, ClientEnv)
 
-import Control.Applicative ((<|>))
-import Data.Text (Text)
 import qualified Data.Text as T
 
-import CheckCheck.Contracts.Receipts (ReceiptsAPI, ReceiptResp (ReceiptResp), ReceiptItemResp (..))
-import Data.Proxy (Proxy(..))
+import CheckCheck.Contracts.Receipts (ReceiptsAPI, ReceiptResp (..), ReceiptItemResp (..))
+import Control.Applicative ((<|>))
 import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.Reader (ReaderT (ReaderT, runReaderT), MonadReader (ask))
+import Control.Monad (unless)
+import Data.Text (Text)
 import Data.Functor ((<&>))
-import SmartPrimitives.Positive (pattern Positive)
+import Data.Proxy (Proxy(..))
 import Network.HTTP.Client (defaultManagerSettings, newManager)
+import SmartPrimitives.Positive (pattern Positive)
 
 getReceipt :: Text -> ClientM ReceiptResp
 getReceipt = client $ Proxy @ReceiptsAPI
 
-newtype Model = Model
-  { botName :: BotName }
-  deriving Show
+data State
+  = InitialState
+  | SelectingReceiptItems Text [Int]
 
-data Action
-  = NoAction
-  | ShowReceiptQr Text
-  deriving Show
+data Transition
+  = ShowReceipt Text
+  | SelectReceiptItem Int
+  | FinishSelectingReceiptItems
+  deriving (Read, Show)
 
-bot :: BotName -> BotApp Model Action
-bot botName = BotApp
-  { botInitialModel = Model botName
-  , botAction = flip handleUpdate
-  , botHandler = handleAction
+mkBotApp :: ClientEnv -> BotName -> BotApp State Transition
+mkBotApp clientEnv botName = BotApp
+  { botInitialModel = InitialState
+  , botAction = flip $ decideTransition botName
+  , botHandler = botHandler
   , botJobs = []
   }
-
-handleUpdate :: Model -> Update -> Maybe Action
-handleUpdate Model{ botName = BotName botName } = parseUpdate
-  $ ShowReceiptQr <$> (command' "qr" <|> command' "receipt")
   where
-    command' =
-       liftA2 (<|>) command (commandWithBotName botName)
+    botHandler action model = nt clientEnv $ handleTransition action model
+    nt :: ClientEnv -> Eff' action model -> Eff action model
+    nt = flip runReaderT
 
-handleAction :: Action -> Model -> Eff Action Model
-handleAction NoAction model = pure model
+newtype AppM a = AppM { _runAppM :: ReaderT ClientEnv BotM a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadReader ClientEnv)
 
-handleAction (ShowReceiptQr qr) model = model <# do
-  manager <- liftIO $ newManager defaultManagerSettings
-  result <- liftIO $ runClientM (getReceipt qr) (mkClientEnv manager (BaseUrl Http "localhost" 8080 "receipts"))
-  case result of
-    Left err -> liftIO $ putStrLn $ "Error: " <> show err
-    Right (ReceiptResp items) -> do
-      let msg = T.unlines $ items <&>
-            \ReceiptItemResp{ name, quantity = Positive quantity, price = Positive price }
-              -> name
-              <> " в количестве "
-              <> T.pack (show quantity)
-              <> "; в сумме на "
-              <> T.pack (show $ fromIntegral price * quantity / 100)
-              <> " рублей"
-      replyText msg
-  return NoAction
+runReq :: ClientM a -> AppM (Either ClientError a)
+runReq req = do
+  clientEnv <- ask
+  liftIO $ runClientM req clientEnv
+
+tg :: BotM a -> AppM a
+tg = AppM . ReaderT . const
+
+type Eff' action = ReaderT ClientEnv (Eff action)
+
+tshow :: Show a => a -> Text
+tshow = T.pack . show
+
+(<#) :: GetAction a action => state -> AppM a -> Eff' action state
+state <# (AppM app) = do
+  clientEnv <- ask
+  let bot = runReaderT app clientEnv
+  ReaderT $ const $ withEffect bot state
+
+divide :: Int -> Double -> Double
+divide a b = fromIntegral a / b
+
+process :: [ReceiptItemResp] -> [(Int, Text)]
+process = fmap
+  \ReceiptItemResp{ index, name, quantity = Positive quantity, price = Positive price } ->
+    ( index
+    , tshow index
+      <> ". "
+      <> T.take 20 name
+      <> "... x "
+      <> tshow quantity
+      <> " = "
+      <> tshow (round (fromIntegral price * quantity) `divide` 100)
+      <> " rub"
+    )
+
+decideTransition :: BotName -> State -> Update -> Maybe Transition
+decideTransition (BotName botName) state = parseUpdate parser
+  where
+    command' = liftA2 (<|>) command (commandWithBotName botName)
+
+    parser = case state of
+      InitialState -> ShowReceipt <$> (command' "qr" <|> command' "receipt")
+
+      SelectingReceiptItems _ _ -> do
+        transition <- callbackQueryDataRead
+        unless (isAllowed transition) $
+          fail "unsupported transition"
+        return transition
+          where
+            isAllowed (SelectReceiptItem _) = True
+            isAllowed FinishSelectingReceiptItems = True
+            isAllowed _ = False
+
+handleTransition :: Transition -> State -> Eff' Transition State
+handleTransition transition InitialState = case transition of
+  ShowReceipt qr -> SelectingReceiptItems qr [] <# do
+    runReq (getReceipt qr) >>= \case
+      Left err -> liftIO $ putStrLn $ "Error: " <> show err
+      Right (ReceiptResp items) -> do
+        let items' = process items
+        let confirmButton = actionButton "Confirm" FinishSelectingReceiptItems
+        let itemsButtons = items'
+              <&> (: [])
+               .  \(index, item) -> actionButton item (SelectReceiptItem index)
+        let buttons = itemsButtons ++ [[confirmButton]]
+        let msg' = "Scanned receipt items: "
+              { replyMessageReplyMarkup = Just $ SomeInlineKeyboardMarkup $ InlineKeyboardMarkup
+                { inlineKeyboardMarkupInlineKeyboard = buttons }
+              }
+        tg $ reply msg'
+  _ -> undefined
+
+handleTransition action (SelectingReceiptItems qr indices) = case action of
+  SelectReceiptItem index ->
+    let indices' = index:indices in
+    SelectingReceiptItems qr indices' <# do
+    runReq (getReceipt qr) >>= \case
+      Left err -> liftIO $ putStrLn $ "Error: " <> show err
+      Right (ReceiptResp items) -> do
+        let items' = process items
+        let confirmButton = actionButton "Confirm" FinishSelectingReceiptItems
+        let itemsButtons = items'
+              <&> (: [])
+               .  \(i, item) -> actionButton item (SelectReceiptItem i)
+        let buttons = itemsButtons ++ [[confirmButton]]
+        let editMessage' = EditMessage
+              { editMessageText = "selected items: " <> tshow indices'
+              , editMessageParseMode = Nothing
+              , editMessageLinkPreviewOptions = Nothing
+              , editMessageReplyMarkup = Just $ SomeInlineKeyboardMarkup $ InlineKeyboardMarkup
+                { inlineKeyboardMarkupInlineKeyboard = buttons }
+              }
+        tg $ editUpdateMessage editMessage'
+  FinishSelectingReceiptItems -> InitialState <# do
+    runReq (getReceipt qr) >>= \case
+      Left err -> liftIO $ putStrLn $ "Error: " <> show err
+      Right (ReceiptResp items) -> do
+        let filteredItems = filter (flip elem indices . index) items
+        let overallSum = (`divide` 100) $ sum $ filteredItems <&>
+              \ReceiptItemResp{ quantity = Positive quantity, price = Positive price } ->
+                round (fromIntegral price * quantity)
+        let msgLines
+              = ("В сумме на: " <> tshow overallSum)
+              : (snd <$> process filteredItems)
+        tg $ replyText $ T.unlines msgLines
+  _ -> undefined
 
 run :: Token -> IO ()
 run token = do
-  env <- defaultTelegramClientEnv token
-  mUsername <- either (error . show) (userUsername . responseResult) <$> runClientM getMe env
-  let botName = maybe (error "bot name is not defined") BotName mUsername
-  startBot_ (bot botName) env
+  tgEnv <- defaultTelegramClientEnv token
+  mBotName <- either (error . show) (userUsername . responseResult) <$> runClientM getMe tgEnv
+  let botName = maybe (error "bot name is not defined") BotName mBotName
+
+  manager <- newManager defaultManagerSettings
+  let clientEnv = mkClientEnv manager (BaseUrl Http "localhost" 8080 "receipts")
+
+  let botApp = conversationBot updateChatId $ mkBotApp clientEnv botName
+  startBot_ botApp tgEnv
 
 main :: IO ()
 main = do
