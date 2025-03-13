97d74871d17285c5b72fbe1c6caf46e43ae5c45a
danielambda <daniel.gevorgyan25@gmail.com>
2025-03-13 03:28:03 +0300

    chore: updated telegram bot api clients

--- telegram-bot/src/Clients.hs
+++ telegram-bot/src/Clients.hs
@@ -5,6 +5,7 @@ module Clients
   ( ApiClient(..)
   , GroupsClient(..)
   , UsersClient(..)
+  , ContactsClient(..)
   , OutgointRequestsClient(..)
   , apiClient
   ) where
@@ -12,7 +13,7 @@ module Clients
 import Data.Proxy (Proxy(..))
 import Data.Text (Text)
 import Data.UUID (UUID)
-import Servant.API ((:<|>)(..))
+import Servant.API ((:<|>)(..), NoContent)
 import Servant.Auth.Client (Token)
 import Servant.Client (ClientM, client)
 
@@ -21,6 +22,7 @@ import CheckCheck.Contracts.Groups (CreateGroupReqBody, GroupResp)
 import CheckCheck.Contracts.Receipts (ReceiptResp)
 import CheckCheck.Contracts.Users (UserResp)
 import CheckCheck.Contracts.Users.OutgoingRequests (SendRequestReqBody, RequestResp)
+import CheckCheck.Contracts.Users.Contacts (CreateContactReqBody, ContactResp)
 
 data ApiClient = ApiClient
   { getReceipt :: Text -> ClientM ReceiptResp
@@ -36,7 +38,14 @@ data GroupsClient = GroupsClient
 
 data UsersClient = UsersClient
   { getMe :: ClientM UserResp
+  , contactsClient :: ContactsClient
   , outgoingRequestsClient :: OutgointRequestsClient
+  }
+
+data ContactsClient = ContactsClient
+  { createContact :: CreateContactReqBody -> ClientM ContactResp
+  , getContacts :: ClientM [ContactResp]
+  , deletaContact :: UUID -> ClientM NoContent
   }
 
 newtype OutgointRequestsClient = OutgointRequestsClient
@@ -53,7 +62,11 @@ apiClient = ApiClient{..}
 
     mkUsersClient token = UsersClient{..}
       where
-        getMe :<|> _ :<|> outgoingRequestsClient' :<|> _ = usersClient token
+        getMe :<|> contactsClient' :<|> outgoingRequestsClient' :<|> _ = usersClient token
+
+        contactsClient = ContactsClient{..}
+          where
+            getContacts :<|> createContact :<|> deletaContact = contactsClient'
 
         outgoingRequestsClient = OutgointRequestsClient{..}
           where
